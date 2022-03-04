f20730706f4f88d9abaf9b39cb147786c3fbd407
Lambda <poberezhetspavlo@gmail.com>
2022-03-03 20:56:06 -0500

    add roality

--- contracts/FathomyachtClub.sol
+++ contracts/FathomyachtClub.sol
@@ -4,9 +4,10 @@ pragma solidity >=0.8.9 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
-contract FathomyachtClub is ERC721URIStorage, Ownable {
+contract FathomyachtClub is ERC721URIStorage, ERC2981, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
   uint256 private MAX_TOKEN = 8000;
@@ -25,17 +26,41 @@ contract FathomyachtClub is ERC721URIStorage, Ownable {
     "https://gateway.pinata.cloud/ipfs/QmXn2VsBujETLvCKdy7kHzEVevQ6cBrp6Nc9WgMd9ggQKr"
   ];
 
-  // mapping for tier to price(1: power, 2: yacht, 3: prestige)
+  // mapping for tier to price(0: power, 1: yacht, 2: prestige)
   uint256[] private NFT_PRICE = [0, 0, 0];
-  uint8[] private TIER_MINT_LIMIT = [100, 100, 50];
+  uint256[] private PRESALE_TIER_MINT_LIMIT = [100, 100, 50];
+  uint256[] private PUBLIC_SALE_TIER_MINT_LIMIT = [900, 900, 950];
+  // 0: presale start block, 1: public sale start block
+  uint256[] private EVENT_BLOCK = [10264904, 10437473];
+  // Mapping from address to minted number of NFTs(0: power, 1: yacht, 2: prestige)
+  mapping(uint8 => uint256) private _preSaleMintCounter;
+  // Mapping from address to minted number of NFTs(0: power, 1: yacht, 2: prestige)
+  mapping(uint8 => uint256) private _publicSaleMintCounter;
   // Mapping from address to white list flag(1: added, 0: removed)
   mapping(address => uint8) private _whitelister;
 
   constructor() ERC721("Fathom Yacht Club", "FYC") {}
 
-  modifier ableBatch(uint8 tierNumber) {
+  modifier ableMintBatch(uint256 number, uint8 tierNumber) {
+    uint8 blockStatus = checkBlock(msg.sender);
+    require(blockStatus > 0, "Not available to mint.");
     require(keccak256(abi.encodePacked(_batchTokenURI[tierNumber])) != keccak256(abi.encodePacked("")), "Batch Token URI not set");
     _;
+  }
+
+  function checkBlock(address pm_address) internal view returns(uint8) {
+    uint256 curBlock = block.number;
+
+    if (curBlock >= EVENT_BLOCK[1]) {
+      return 2;
+    }
+    if (curBlock >= EVENT_BLOCK[0]) {
+      if(_whitelister[pm_address]== 1) {
+        return 1;
+      }
+    }
+
+    return 0;
   }
 
   function addWhiteList(address[] memory lsts) public onlyOwner {
@@ -52,18 +77,18 @@ contract FathomyachtClub is ERC721URIStorage, Ownable {
     }
   }
 
-  function getTierPrice(uint8 tierNumber) external view onlyOwner returns(uint256) {
-    require(tierNumber >= 1 && tierNumber <= 3, "Invalied tierNumber of array");
+  function getTierPrice(uint8 tierNumber) external view returns(uint256) {
+    require(tierNumber >= 0 && tierNumber <= 2, "Invalied tierNumber of array");
     return NFT_PRICE[tierNumber];
   }
 
   function setTierPrice(uint256 price, uint8 tierNumber) external onlyOwner {
-    require(tierNumber >= 1 && tierNumber <= 3, "Invalied tierNumber of array.");
+    require(tierNumber >= 0 && tierNumber <= 2, "Invalied tierNumber of array.");
     NFT_PRICE[tierNumber] = price;
   }
 
-  function getTokenBatchURI(uint8 tierNumber) external view onlyOwner returns(string memory) {
-    require(tierNumber >= 1 && tierNumber <= 3, "Invalied tierNumber of array.");
+  function getTokenBatchURI(uint8 tierNumber) external view returns(string memory) {
+    require(tierNumber >= 0 && tierNumber <= 2, "Invalied tierNumber of array.");
     return _batchTokenURI[tierNumber];
   }
 
@@ -80,6 +105,32 @@ contract FathomyachtClub is ERC721URIStorage, Ownable {
     MAX_TOKEN = limit;
   }
 
+  function getTierLimit(uint8 tierNumber) external view returns(uint256) {
+    require(tierNumber >= 0 && tierNumber <= 2, "Invalied tierNumber of array.");
+
+    uint256 curBlock = block.number;
+    if (curBlock >= EVENT_BLOCK[1]) {
+      return PUBLIC_SALE_TIER_MINT_LIMIT[tierNumber];
+    }
+    if (curBlock >= EVENT_BLOCK[0]) {
+      return PRESALE_TIER_MINT_LIMIT[tierNumber];
+    }
+
+    return 0;
+  }
+
+  function setTierMintLimit(uint256 limit, uint8 tierNumber) external onlyOwner {
+    require(tierNumber >= 0 && tierNumber <= 2, "Invalied tierNumber of array.");
+
+    uint256 curBlock = block.number;
+    if (curBlock >= EVENT_BLOCK[1]) {
+      PUBLIC_SALE_TIER_MINT_LIMIT[tierNumber] = limit;
+    }
+    if (curBlock >= EVENT_BLOCK[0]) {
+      PRESALE_TIER_MINT_LIMIT[tierNumber] = limit;
+    }
+  }
+
   function withDraw() external onlyOwner {
     address payable tgt = payable(owner());
     (bool success1, ) = tgt.call{value:address(this).balance}("");
@@ -90,23 +141,57 @@ contract FathomyachtClub is ERC721URIStorage, Ownable {
     _setTokenURI(number, tokenURI);
   }
 
-  function mintBatch(uint256 number, uint8 tierNumber) public payable ableBatch(tierNumber-1) returns(uint256) {
-    uint8 maxBatchable = 2;
-    require(tierNumber >= 1 && tierNumber <= 3, "Invalied tierNumber of array.");
+  function mintBatch(uint256 number, uint8 tierNumber) public payable ableMintBatch(number, tierNumber) returns(uint256) {
+    require(tierNumber >= 0 && tierNumber <= 2, "Invalied tierNumber of array.");
     require(NFT_PRICE[tierNumber] != 0, "Tier price is not set.");
-    require(number < maxBatchable, "You are not allowed to buy more than 2 tokens at once.");
     require(MAX_TOKEN > number + _tokenIds.current() + 1, "Not enough tokens left to buy.");
     require(msg.value >= NFT_PRICE[tierNumber] * number, "Amount of ether sent not correct.");
+
+    uint8 maxBatchable = 2;
+    require(number < maxBatchable, "You are not allowed to buy more than 2 tokens at once.");
+
+    uint256[] storage tierMintLimit = PRESALE_TIER_MINT_LIMIT;
+    mapping(uint8 => uint256) storage tierMintCounter = _preSaleMintCounter;
+    uint256 curBlock = block.number;
+
+    // use public sale variables if the current date is after public sale started
+    if (curBlock >= EVENT_BLOCK[1]) {
+      tierMintLimit = PUBLIC_SALE_TIER_MINT_LIMIT;
+      tierMintCounter = _publicSaleMintCounter;
+    }
+    require(tierMintCounter[tierNumber] + number < tierMintLimit[tierNumber], "Overflow maximum mint limitation.");
+
+    tierMintCounter[tierNumber] = tierMintCounter[tierNumber] + number;
 
     uint256 newItemId = 0;
     for (uint256 i = 0; i < number; i++) {
       _tokenIds.increment();
       newItemId = _tokenIds.current();
       _mint(msg.sender, newItemId);
-      _setTokenURI(newItemId, _batchTokenURI[tierNumber - 1]);
+      _setTokenURI(newItemId, _batchTokenURI[tierNumber]);
       _tokenToTier[newItemId] = tierNumber;
     }
 
     return newItemId;
+  }
+
+  function totalSupply() public view returns(uint256) {
+    return _tokenIds.current();
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC2981) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+
+  /**
+    * @dev See {ERC721-_burn}. This override additionally clears the royalty information for the token.
+    */
+  function _burn(uint256 tokenId) internal virtual override {
+    super._burn(tokenId);
+    _resetTokenRoyalty(tokenId);
+  }
+
+  function setRoality(address receiver, uint96 feeNumerator) external {
+    _setDefaultRoyalty(receiver, feeNumerator);
   }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -41,11 +41,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -139,6 +139,6 @@ module.exports = {
   // }
   plugins: ['truffle-plugin-verify'],
   api_keys: {
-    etherscan: '6M7MCMPRTHH78TACVYCBV1A4SKC7JQZKU4'
+    etherscan: '9976Y89G1IDQ2KZF9F4ZENDND1FE3W2M8E'
   }
 };
